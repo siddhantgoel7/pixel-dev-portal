dd649f75fe541fa62e99bf5a0dc6e602ac4bc53d
siddhantgoel7 <siddhantgoel5@gmail.com>
2025-07-03 15:35:33 +0530

    update to deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { resolve } from "path";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: "/portfolio-sg/",
   server: {
@@ -12,9 +13,23 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: resolve(__dirname, "dist/index.html"),
+          dest: "", // will be copied to dist/404.html later in script
+        }
+      ]
+    })
   ].filter(Boolean),
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html")
+      }
+    }
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
