527ac76311d9025920321ea9331600471906bbd8
siddhantgoel7 <siddhantgoel5@gmail.com>
2025-07-03 15:26:25 +0530

    Github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/portfolio-sg/",
   server: {
     host: "::",
     port: 8080,
