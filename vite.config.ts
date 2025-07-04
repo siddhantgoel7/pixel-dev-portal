761719c27be695905ade5493cad5d8e8e880269b
siddhantgoel7 <siddhantgoel5@gmail.com>
2025-07-04 13:26:31 +0530

    more updates to gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import { resolve } from "path";
 
 export default defineConfig(({ mode }) => ({
   base: "/portfolio-sg/",
@@ -13,15 +12,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     mode === 'development' && componentTagger(),
-    
   ].filter(Boolean),
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html")
-      }
-    }
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
